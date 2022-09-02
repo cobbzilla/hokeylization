dc7f65892b540fe2e3a4a95a8e1d07258a37ff57
Jonathan Cobb <jonathan@kyuss.org>
2022-09-02 09:54:46 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+Hokeylization
+=============
+The name is a portmanteau, meaning 'hokey localization'
+
+It's hokey because it's very simple: it runs some strings against Google Translate
+
+You can translate:
+* a JavaScript object containing messages
+* a directory of files, recursively
+
+## Source
+* [hokeylization on GitHub](https://github.com/cobbzilla/hokeylization)
+* [hokeylization on npm](https://www.npmjs.com/package/hokeylization)
+
+## Setup
+Set the `GOOGLE_TRANSLATE_PROJECT_ID` env var to identify your project
+
+Set the `GOOGLE_APPLICATION_CREDENTIALS` env var to the JSON credentials you downloaded
+after navigating the holy hell that is figuring out authentication on Google cloud
+
+If you're running from source, you can also put these in a `.env` file in the source directory
+they'll be loaded at runtime via [dotenv](https://www.npmjs.com/package/dotenv)
+
+## Translating a JavaScript string resource file
+Your string table **must** be in a JavaScript file in the form:
+
+    export default {
+      string_key: "some value",
+      another_key: "another value",
+      ... more keys ...
+    }
+
+If this file was named `myfile.json`, you can translate it to Spanish with:
+
+    hokey -L es -o myfile.es.json myfile.json
+
+For the `-L` / `--language` option, you can use any 2-letter language code
+[supported by Google Translate](https://cloud.google.com/translate/docs/languages)
+
+## Translating a directory of text files
+You can also translate a directory, hokey will recursively visit every file
+in the directory and run its contents through Google Translate, and save the output
+to an identically named file in a separate directory tree
+
+When the target of your translation is a directory, this mode is enabled
+
+The `-o` / `--outfile` option specifies the output directory
+
+**BIG WARNING**: When translating directories, **DO NOT** specify an output directory
+that is within your input directory! If you do, you will:
+* induce infinite recursion
+* run up your Google bill
+* fill up your disk
+* have less fun
+
+Here is an example of what *not to do*:
+
+    hokey -L es -o templates/es templates    # <--- DON'T DO THIS!
+
+When this runs, translated files are written to `templates/es`, and thus become new
+source files to translate, since they are under `templates/` -- this process continues
+forever, don't do it!
+
+OK, let's say you have some email templates in a directory:
+
+    templates/email/en/welcome.txt
+    templates/email/en/welcome.html
+    templates/email/en/verify-account.txt
+    templates/email/en/verify-account.html
+    templates/email/en/reset-password.txt
+    templates/email/en/reset-password.html
+
+To translate all of these to Spanish, run:
+
+    hokey -L es -o templates/email/es templates/email/en
+
+What happens when the above runs:
+* The `templates/email/es` will be created if it doesn't exist
+* Every file in `templates/email/en` will be translated
+* You'll end up with an identical directory structure under `es` that you have under `en`
+
+## Happy Translating!
---
+++ index.js
@@ -0,0 +1,139 @@
+require('dotenv').config()
+const fs = require('fs')
+const path = require('path')
+const commander = require('commander')
+
+/**
+ * TODO(developer): Uncomment the following line before running the sample.
+ */
+const projectId = process.env.GOOGLE_TRANSLATE_PROJECT_ID
+
+// Imports the Google Cloud client library
+const { Translate } = require('@google-cloud/translate').v2
+
+const VERSION = require('./package.json').version;
+
+// adapted from https://stackoverflow.com/a/26828357/1251543
+const processDirectory = (jsFile, lang, outfile) => {
+    const outfileStat = fs.lstatSync(jsFile, {throwIfNoEntry: false})
+    if (!outfileStat) {
+        fs.mkdirSync(outfile, {recursive: true})
+    } else if (!outfileStat.isDirectory()) {
+        throw new TypeError(`processDirectory: not a directory: ${outfile}`)
+    }
+
+    const translate = new Translate({projectId})
+    const visitor = async (fullPath) => {
+        const contents = fs.readFileSync(fullPath, 'utf8')
+        const [translation] = await translate.translate(contents, lang)
+        let translationFile
+        if (fullPath.includes(jsFile)) {
+            const start = fullPath.indexOf(jsFile)
+            const relativePath = fullPath.substring(start + jsFile.length)
+            translationFile = path.resolve(path.join(outfile, relativePath))
+        } else {
+            translationFile = path.resolve(path.join(outfile, fullPath.replaceAll('/', '_')))
+        }
+        fs.mkdirSync(path.dirname(translationFile), {recursive: true})
+        fs.writeFileSync(translationFile, translation)
+        console.log(`WROTE: ${translationFile}`)
+    }
+    const walk = (directoryName) => {
+        fs.readdir(directoryName, (e, files) => {
+            if (e) {
+                console.log('Error: ', e)
+                return
+            }
+            for (const file of files) {
+                const fullPath = path.join(directoryName,file);
+                fs.stat(fullPath, (e, f) => {
+                    if (e) {
+                        console.log('Error: ', e)
+                        return
+                    }
+                    if (f.isDirectory()) {
+                        walk(fullPath)
+                    } else {
+                        visitor(fullPath).then(
+                            () => {},
+                            (err) => {
+                                console.error(`Error translating ${fullPath}: ${err}`)
+                            }
+                        )
+                    }
+                })
+            }
+        })
+    }
+    walk(jsFile)
+}
+
+const program = new commander.Command()
+    .command('hokey')
+    .summary(`Quick translation service using Google Translate\nVersion ${VERSION}`)
+    .description('Set the GOOGLE_TRANSLATE_PROJECT_ID environment variable to your Google Translate project\n' +
+        'Set the GOOGLE_APPLICATION_CREDENTIALS environment variable to point to your credentials JSON file')
+    .requiredOption('-L, --language <lang>', '2-letter code for language to translate into')
+    .requiredOption('-O, --outfile <out-file>', 'Write JS output to this file. Default is input filename with lang extension\n' +
+        'For directory processing, this is the output directory. It will be created if it does not exist')
+    .argument('<js-file>', 'A JS file that exports strings to translate\nOr, if a directory, a directory filled with text files to translate')
+    .version(VERSION)
+    .showHelpAfterError()
+    .action(async (jsFile, opts) => {
+        if (opts.language.length !== 2) {
+            throw new TypeError(`lang must be 2 characters: ${opts.language}`)
+        }
+        const lang = opts.language.toLowerCase()
+        const stat = fs.lstatSync(jsFile)
+        if (stat.isDirectory()) {
+            await processDirectory(jsFile, lang, opts.outfile || `./${path.basename(jsFile)}_${lang}_${Date.now()}`)
+            return
+        }
+
+        const file = fs.readFileSync(jsFile).toString('utf8').trim()
+        let keys
+        if (file.startsWith('export default ')) {
+            const firstCurly = file.indexOf('{')
+            if (firstCurly === -1) throw new TypeError(`invalid json in ${jsFile}`)
+            keys = eval('Object.assign({}, ' + file.substring(firstCurly) + ')')
+        } else {
+            keys = require(jsFile)
+        }
+        let result = "export default {"
+        let first = true
+        try {
+            // create a client
+            const translate = new Translate({projectId})
+
+            for (const key of Object.keys(keys)) {
+                // Translates some text into Russian
+                const [translation] = await translate.translate(keys[key], lang)
+                if (first) {
+                    result += '\n'
+                    first = false
+                } else {
+                    result += ',\n'
+                }
+                result += `  ${key}: '${translation.replaceAll("'", "\\'")}'`
+            }
+            result += '\n}\n'
+        } catch (e) {
+            console.error(` **** Error translating: ${e}`)
+        }
+
+        const outfile = opts.outfile || lang + '_' + Date.now() + '_' + jsFile
+        try {
+            fs.writeFileSync(outfile, result)
+        } catch (e) {
+            console.error(` **** Error writing to outfile ${opts.outputFile}: ${e}`)
+        }
+    })
+
+const args = process.argv
+
+try {
+    program.parse(args)
+} catch (e) {
+    console.error(`FATAL ERROR: ${e}`)
+    throw e
+}
